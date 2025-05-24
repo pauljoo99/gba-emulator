3fb87046015b493bf8a1fd935a67c1439cb80cd1
Paul Joo <pauljoold@gmail.com>
2025-05-24 09:26:02 -0700

    Get rid of unnecessary include cstdint

--- thumb2_instructions.h
+++ thumb2_instructions.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cstdint>
 #include <cstdlib>
 #include <stdio.h>
 
--- thumb_instructions.h
+++ thumb_instructions.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "datatypes.h"
-#include <cstdint>
 
 namespace Emulator::Thumb {
 
