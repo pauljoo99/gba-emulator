770d844f9c2b60e21e73401b58be145f8f17304d
Paul Joo <pauljoold@gmail.com>
2025-06-18 16:55:25 -0700

    Add in some util to log when dispcnt changes

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 
 void MockDisplayLoop(Emulator::Memory::Memory *memory) {
+  U16 dispcnt = 0;
   while (true) {
     // Write to VCOUNT
     U8 val = ReadByteFromGBAMemory(*memory, 0x04000006);
@@ -12,6 +13,12 @@ void MockDisplayLoop(Emulator::Memory::Memory *memory) {
     // Runs a 60Hz. 1/60 seconds per screen, 228 scanlines. Each scanline takes
     // 73 microsconds.
     std::this_thread::sleep_for(std::chrono::microseconds(73));
+
+    U16 dispcnt_new = ReadHalfWordFromGBAMemory(*memory, 0x04000000);
+    if (dispcnt != dispcnt_new) {
+      LOG("dispcnt: 0x%04x", dispcnt_new);
+      dispcnt = dispcnt_new;
+    }
   }
 }
 
